a25143d44c326cc5822a6034e6c6745e576d9ba9
katty <oboyshchik@yandex.ru>
2020-11-05 09:32:49 +0300

    ассоциацивные массивы без компаратора

--- src/ExperimentsWithAssociativeArrays.java
+++ src/ExperimentsWithAssociativeArrays.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 
 public class ExperimentsWithAssociativeArrays {
@@ -7,8 +8,9 @@ public class ExperimentsWithAssociativeArrays {
         System.out.println(transliteration("Юлия"));
         System.out.println(countChar("BananaChou"));
         System.out.println(statChar("BananaChou"));
-        System.out.println(freqWords("resources/pushkin.txt"));
+        freqWordsMap("resources/pushkin.txt");
     }
+
     public static Map<Character, String> createTransliterationTable() {
         char[] rus = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
         String[] eng = {"a", "b", "v", "g", "d", "e", "e", "zh", "z", "i", "i", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "ie", "y", "", "e", "iu", "ia"};
@@ -34,8 +36,8 @@ public class ExperimentsWithAssociativeArrays {
 
     public static Map<Character, Integer> countChar(String s) {
         Map<Character, Integer> freq = new HashMap<>();
-        String normStr= String.valueOf(s).toLowerCase();
-        for (int i=0; i<normStr.length(); i++) {
+        String normStr = String.valueOf(s).toLowerCase();
+        for (int i = 0; i < normStr.length(); i++) {
             Character symb = normStr.charAt(i);
             freq.put(symb, freq.getOrDefault(symb, 0) + 1);
         }
@@ -43,31 +45,36 @@ public class ExperimentsWithAssociativeArrays {
     }
 
 
-    public static  Map<Character, List<Integer>> statChar(String s) {
+    public static Map<Character, List<Integer>> statChar(String s) {
         Map<Character, List<Integer>> freq = new HashMap<>();
-        String normStr= String.valueOf(s).toLowerCase();
-        for (int i=0; i<normStr.length(); i++) {
+        String normStr = String.valueOf(s).toLowerCase();
+        for (int i = 0; i < normStr.length(); i++) {
             Character symb = normStr.charAt(i);
             List<Integer> index = new ArrayList<>();
-            freq.getOrDefault(symb, index).add(i+1);
+            freq.getOrDefault(symb, index).add(i + 1);
             freq.put(symb, freq.getOrDefault(symb, index));
         }
         return freq;
     }
 
+    public static void freqWordsMap(String s) throws FileNotFoundException {
+        Map<String, Integer> type1 = new HashMap<>();
+        Map<String, Integer> type2 = new TreeMap<>();
+        Map<String, Integer> type3 = new LinkedHashMap<>();
+        freqWords(type1, s);
+        freqWords(type2, s);
+        freqWords(type3, s);
+    }
 
-    private static Map<String, Integer> freqWords(String fileName) throws FileNotFoundException {
-        Map<String, Integer> freq = new HashMap<>();
+    private static void freqWords(Map<String, Integer> freq, String fileName) throws FileNotFoundException {
         File f = new File(fileName);
-        try (Scanner in = new Scanner(f, "utf8").useDelimiter("[(.*?;:\\{\\}\\[\\]!\\\"\\r\\n\\,\\\\»\\\\«\\\\—\\s]+")) {
+        try (Scanner in = new Scanner(f, "utf8").useDelimiter("[(.*?;:\\(\\)\\{\\}\\[\\]!\\\"\\r\\n\\,\\\\»\\\\«\\\\—\\s]+")) {
             while (in.hasNext()) {
                 String temp = in.next();
                 freq.put(temp, freq.getOrDefault(temp, 0) + 1);
             }
         }
-        //сделать сортировку
-        //добавить разные виды Map
-        return freq;
+        System.out.println(freq);
     }
 
 
